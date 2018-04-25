41928143638d6b5e28f7637a3008e0001893923a
d-kang <david.kang714@gmail.com>
2018-04-24 21:19:16 -0700

    [2-ARP] Render Props Complete

--- 2-advanced-react-patterns-v2/src/__tests__/01.js
+++ 2-advanced-react-patterns-v2/src/__tests__/01.js
@@ -22,8 +22,8 @@ test('renders a toggle component', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=01&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
+// test.skip('I submitted my elaboration and feedback', () => {
+//   const submitted = false // change this when you've submitted!
+//   expect(submitted).toBe(true)
+// })
 ////////////////////////////////
