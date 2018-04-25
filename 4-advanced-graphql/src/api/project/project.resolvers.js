c509cebc237e023b18fa456b01434005fef3470a
d-kang <david.kang714@gmail.com>
2018-04-25 12:50:47 -0700

    [4-AGQL] Lesson-1 Complete

--- 4-advanced-graphql/src/api/project/project.resolvers.js
+++ 4-advanced-graphql/src/api/project/project.resolvers.js
@@ -1,6 +1,14 @@
 // resolve the query you made
 module.exports = {
   Query: {
-    
+    helloWorld() {
+      return {
+        message:'World!'
+      }
+    }
   }
 }
+
+// type Query {
+//   onePerson(id: ID!): Person!
+// }
